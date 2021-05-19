0cd6361c1655b8aa9a8562890a510abb776dd9d7
jenjenayjen <jenny.louie.1231@gmail.com>
2021-05-19 16:55:01 -0700

    added projects 2 folder
    includes index.html and main.js files

---
+++ Basic-JavaScript-Projects/Projects2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//step 68
+function returnSth() {
+    var str = "Hi Im a string.";
+    var result = str.fontcolor("green");
+    document.getElementById("say-sth").innerHTML = result;
+}
+//step 70
+function concat() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
