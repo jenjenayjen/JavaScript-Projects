7d45d6fbd984cfd3dab4eb47608c1ca220c83e9b
jenjenayjen <jenny.louie.1231@gmail.com>
2021-05-19 16:56:05 -0700

    Update main.js

--- Basic-JavaScript-Projects/Projects2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Projects2_functions/JS/main.js
@@ -1,10 +1,10 @@
-//step 68
+//step 68 returns a string
 function returnSth() {
     var str = "Hi Im a string.";
     var result = str.fontcolor("green");
     document.getElementById("say-sth").innerHTML = result;
 }
-//step 70
+//step 70 returns a concatenated string
 function concat() {
     var sentence = "I am learning";
     sentence += " a lot from this course!";
